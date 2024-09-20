baa954efd04ad2789205f16a9efbb5fac53d7311
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-09-20 09:52:18 +0000

    chore: update work experience

--- src/app/resume/_components/work-experience.tsx
+++ src/app/resume/_components/work-experience.tsx
@@ -17,37 +17,37 @@ export const WorkExperience: FC = () => {
           <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-black dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              Nov 2020 - May 2024
+              Aug 2024 - Present
             </time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Rajalakshmi Engineering College
+              UST Global, Trivandrum
             </h3>
             <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-              Bachelors in Computer Science and Business Systems (CGPA - 7.9)
+              Developer I - Software Engineering
             </p>
           </li>
           <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-black dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              Apr 2019 - Apr 2020
+              Jan 2024 - Feb 2024
             </time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              St. John&apos;s English School and Junior College
+              Matlync
             </h3>
             <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-              HSLC (CGPA - 6.03)
+              Full Stack Next.js Developer Intern
             </p>
           </li>
           <li className="ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-black dark:border-gray-900 dark:bg-gray-700"></div>
             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              Apr 2017 - Apr 2018
+              Aug 2023 - Dec 2023
             </time>
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              St. John&apos;s English School and Junior College
+              Steam Troops Innovation Labs
             </h3>
             <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-              SSLC (CGPA - 7.04)
+              Full Stack Next.js Developer Intern
             </p>
           </li>
         </ol>
