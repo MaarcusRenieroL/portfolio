e23548c6e98dcf14c4f6b8e5b34efbb30cba1a0c
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2025-07-21 08:14:49 +0530

    feat: setup layout for new portfolio design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { Button } from "~/components/ui/button";
+import { Button } from "~/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-full w-full">
-      <h1 className="text-2xl font-bold">Home Page</h1>
-      <Button>Click Me</Button>
+    <div className="max-w-4xl mx-auto p-10 flex flex-col gap-10">
+      <Button className="w-fit">Hola</Button>
+      <p>navbar</p>
+      <p>header ( contains name, description, current role, place )</p>
+      <p>work experience</p>
+      <p>blog</p>
+      <p>projects</p>
+      <p>footer</p>
     </div>
   );
 }
